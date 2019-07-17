8ed13d14a1d22370a28bda7dfb17bdf2d0cc32eb
Ubaldo Brea <ubrea@me.com>
2019-07-17 21:45:59 +0100

    myFilmRentalArray.filmsRented = 20;

--- Mod_01_Practice_03/src/FilmRentalArrayTest.java
+++ Mod_01_Practice_03/src/FilmRentalArrayTest.java
@@ -5,7 +5,7 @@ public class FilmRentalArrayTest {
   FilmRentalArray myFilmRentalArray = new FilmRentalArray();
   
   myFilmRentalArray.setFilmRentalArray();
-  myFilmRentalArray.filmsRented = 1;
+  myFilmRentalArray.filmsRented = 20;
   myFilmRentalArray.displayFilmRentalArray();
   
   myFilmRentalArray.filmsRented = 5;
