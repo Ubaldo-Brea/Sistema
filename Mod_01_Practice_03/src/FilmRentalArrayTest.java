d5b7c1efc434468f688d9bd7200f2f42720363df
Murillo Alves <nomedmca@yahoo.com.br>
2019-07-17 21:39:16 +0100

    multiplicado por 10

--- Mod_01_Practice_03/src/FilmRentalArrayTest.java
+++ Mod_01_Practice_03/src/FilmRentalArrayTest.java
@@ -5,13 +5,13 @@ public class FilmRentalArrayTest {
   FilmRentalArray myFilmRentalArray = new FilmRentalArray();
   
   myFilmRentalArray.setFilmRentalArray();
-  myFilmRentalArray.filmsRented = 1;
-  myFilmRentalArray.displayFilmRentalArray();
-  
-  myFilmRentalArray.filmsRented = 5;
-  myFilmRentalArray.displayFilmRentalArray();
-  
   myFilmRentalArray.filmsRented = 10;
+  myFilmRentalArray.displayFilmRentalArray();
+  
+  myFilmRentalArray.filmsRented = 50;
+  myFilmRentalArray.displayFilmRentalArray();
+  
+  myFilmRentalArray.filmsRented = 100;
   myFilmRentalArray.displayFilmRentalArray();
   
   } 
